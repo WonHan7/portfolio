3588558c4cb30574df3b7e5a41e2631816ab75b3
Wonseok <dannyhan8698@gmail.com>
2022-02-06 21:29:46 -0700

    Added more functionalities to browser window

--- script.js
+++ script.js
@@ -16,8 +16,17 @@ $(function () {
     $("#min").on("click", MinimizeWindow);
     $("#max").on("click", MaximizeWindow);
     $("#exit").on("click", CloseWindow);
+
+    // Grab file name & info
+    $("#ramen-misoya").on("click", RamenMisoyaMenu);
 })
 
+/*****************************
+Function Name: ChangeTheme
+Description: Alternates between light and dark mode
+Parameters: N/A
+Return: No Return
+*****************************/
 function ChangeTheme ()
 {
     if (!Boolean(dark))
@@ -40,6 +49,12 @@ function ChangeTheme ()
     }
 }
 
+/*****************************
+Function Name: GravitateLogo
+Description: "Sucks" all browser windows towards the logo and makes them disappear
+Parameters: N/A
+Return: No Return
+*****************************/
 function GravitateLogo ()
 {
     if (close == 0)
@@ -55,18 +70,52 @@ function GravitateLogo ()
     }
 }
 
+/*****************************
+Function Name: MinimizeWindow
+Description: Docks the browser window on the bottom of the viewport
+Parameters: N/A
+Return: No Return
+*****************************/
 function MinimizeWindow ()
 {
     // Dock it somewhere (Will need to think of an idea)
 }
 
+/*****************************
+Function Name: MaximizeWindow
+Description: Enlarges the specified browser window
+Parameters: N/A
+Return: No Return
+*****************************/
 function MaximizeWindow ()
 {
     // Enlarge and bring it to focus
 }
 
+/*****************************
+Function Name: CloseWindow
+Description: Closes the specified browser window
+Parameters: N/A
+Return: No Return
+*****************************/
 function CloseWindow ()
 {
-    // Close the window
     $("#react").css("display", "none");
+}
+
+/////////////////////////
+/// PROJECT FUNCTIONS ///
+/////////////////////////
+
+/*****************************
+Function Name: RamenMisoyaMenu
+Description: Displays name on input and prepares information for other functions
+Parameters: N/A
+Return: No Return
+*****************************/
+function RamenMisoyaMenu ()
+{
+    $("#file-name").val("Ramen Misoya Menu");
+    $("#ramen-misoya").css("background-color", "#1a67c2");
+    $("#ramen-misoya").css("color", "#FFFFFF");
 }
